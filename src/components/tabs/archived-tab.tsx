1c9ab5833dddb3533adbab713bcb8e9434c7b9f2
amesq01 <amesq1@hotmail.com>
2024-06-04 14:08:27 -0300

    update

--- src/components/tabs/archived-tab.tsx
+++ src/components/tabs/archived-tab.tsx
@@ -11,7 +11,7 @@ export const ArchivedTab = ({processes}:rawDateProps) => {
   return (
     <TabsContent value="archived" className='bg-slate-50/70 h-screen p-0 mt-[1px] px-2'>
       <div className="">
-        <p className="text-center my-10 text-black">Processos Arquivados</p>
+        <p className="text-center my-10 text-black">Processos Arquivados!</p>
 
         <div className=' flex flex-col gap-1'>
           <RenderItens processes={processes} page='archived' />  
